c6c1bdb2a335983d8656e79d35489dc04044dfe7
Aleksandr Chernyshev <sanocher@gmail.com>
2019-05-03 18:24:56 +0500

    build tablebooking fix url

---
+++ tablebooking/precache-manifest.51f500b608b096e5861b005420453d4c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1003f4d745eee75cfe1d",
+    "url": "/tablebooking/static/css/main.006637d7.chunk.css"
+  },
+  {
+    "revision": "1003f4d745eee75cfe1d",
+    "url": "/tablebooking/static/js/main.800245a4.chunk.js"
+  },
+  {
+    "revision": "2d10b6b76ea0702c6302",
+    "url": "/tablebooking/static/js/runtime~main.c84bfdcd.js"
+  },
+  {
+    "revision": "ebd7e9233371f5fb41b8",
+    "url": "/tablebooking/static/css/2.c33ed562.chunk.css"
+  },
+  {
+    "revision": "ebd7e9233371f5fb41b8",
+    "url": "/tablebooking/static/js/2.e235c93e.chunk.js"
+  },
+  {
+    "revision": "5393169c8d790274086b3ab21e05d9ef",
+    "url": "/tablebooking/static/media/dropdown.5393169c.svg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/tablebooking/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/tablebooking/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "cc6557412f1deda45f400932bb6335f1",
+    "url": "/tablebooking/index.html"
+  }
+];
--- tablebooking/service-worker.js
+++ tablebooking/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0db9c784cef3c38c39303cd21d1466cf.js"
+  "/tablebooking/precache-manifest.51f500b608b096e5861b005420453d4c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tablebooking/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
