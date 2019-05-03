087e3dab0aef60794f64dc2f403792b56d684fbb
Aleksandr Chernyshev <sanocher@gmail.com>
2019-05-03 18:23:40 +0500

    build tablebooking

---
+++ tablebooking/precache-manifest.0db9c784cef3c38c39303cd21d1466cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "df7b1d73424b21b46f96",
+    "url": "/static/css/main.1d2e21f6.chunk.css"
+  },
+  {
+    "revision": "df7b1d73424b21b46f96",
+    "url": "/static/js/main.85cfad06.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ebd7e9233371f5fb41b8",
+    "url": "/static/css/2.c33ed562.chunk.css"
+  },
+  {
+    "revision": "ebd7e9233371f5fb41b8",
+    "url": "/static/js/2.e235c93e.chunk.js"
+  },
+  {
+    "revision": "5393169c8d790274086b3ab21e05d9ef",
+    "url": "/static/media/dropdown.5393169c.svg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "69ceac2bb067b98164fa62f72f9f9090",
+    "url": "/index.html"
+  }
+];
